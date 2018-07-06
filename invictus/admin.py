38e92f7b355cc424fda585ce7a502a43225ce5ba
juniorkfelix <felixonsare@gmail.com>
2018-07-06 12:56:23 +0300

    modified admin.py file and added a few files to it

--- invictus/admin.py
+++ invictus/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from invictus.models import Member_reg
+from invictus.models import Contact
+from invictus.models import reg_members
+from invictus.models import Uploads
+from invictus.models import Profile
+from invictus.models import Accounts
+from invictus.models import Transact
+from invictus.models import Jipange_Acc
+from invictus.models import Timiza_Acc
+from invictus.models import Fixed_Acc
+from invictus.models import Help
 
 # Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(Member_reg)
+admin.site.register(reg_members)
+admin.site.register(Uploads)
+admin.site.register(Profile)
+admin.site.register(Accounts)
+admin.site.register(Transact)
+admin.site.register(Jipange_Acc)
+admin.site.register(Timiza_Acc)
+admin.site.register(Fixed_Acc)
+admin.site.register(Help)
